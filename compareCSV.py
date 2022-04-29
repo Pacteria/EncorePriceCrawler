52ca7fd7046806c6e8d2b627511a0c1ee6b22462
Shawn Lin <Pacteria@users.noreply.github.com>
2022-04-29 13:53:20 -0500

    working on compareCSV, prints human-readable text on console

--- compareCSV.py
+++ compareCSV.py
@@ -1,13 +1,14 @@
 from csv_diff import load_csv, compare
 from datetime import date,timedelta
+import json
 
 absPATH = 'E:/Github_repos/EncorePriceCrawler/'
 
 def compareCSV():
-    diff = compare(
-        load_csv(open(absPATH+f'pricingHistory/{date.today() - timedelta(days = 1)}.csv'),key='UnitID'),
-        load_csv(open(absPATH+f'pricingHistory/{date.today()}.csv'),key='UnitID')
-    )
+    prevCSV = load_csv(open(absPATH+f'pricingHistory/{date.today() - timedelta(days = 1)}.csv'),key='UnitID')
+    currCSV = load_csv(open(absPATH+f'pricingHistory/{date.today()}.csv'),key='UnitID')
+    diff = compare(prevCSV,currCSV)
+
     isChanged = False
     for i in diff:
         if len(diff[i]) != 0: 
@@ -16,3 +17,18 @@ def compareCSV():
     
     return diff,isChanged
 
+diff, changed = compareCSV()
+
+for i in diff:
+    if diff[i]:
+        print(f'{len(diff[i])} {i}:')
+        if i == 'changed':
+            for line in diff[i]:
+                UnitID = line['key']
+                changes = line['changes']
+                print(f'    UnitID: {UnitID}')
+                for dataField in changes:
+                    print(f'        {dataField}: {changes[dataField][0]} => {changes[dataField][1]}')
+        else:
+            print(diff[i])
+    print('\n')
